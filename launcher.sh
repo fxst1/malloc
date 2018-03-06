e3feb7249d29789e37bf4ef4c2d57df208785e6c
fxst1 <fxst1@localhost.localdomain>
2018-03-06 10:43:32 +0100

    ft_mem_new

--- launcher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DYLD_LIBRARY_PATH=:libft_malloc_x86_64_Linux.so #MAC
-export LD_PRELOAD=:libft_malloc_x86_64_Linux.so #Linux (prefered)
-export LD_LIBRARY_PATH=:libft_malloc_x86_64_Linux.so #Linux
-echo Starting $@
-$@
